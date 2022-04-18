83ebf0fe97696af189aa72a7e65cf2f6cc45926c
Sabrina Poderis <sabrina.poderis@gmail.com>
2022-04-18 10:18:53 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    
+    './src/pages/**/*.{html,js,ts,jsx,tsx}',
+    './src/components/**/*.{html,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
